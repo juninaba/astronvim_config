4b2d0b4d3ca3580268e478c9839d019a5ef72de8
Junichi Inaba <43479094+juninaba@users.noreply.github.com>
2023-10-16 10:39:59 +0900

    update user.lua

--- plugins/user.lua
+++ plugins/user.lua
@@ -9,4 +9,29 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
+  {
+    "NeogitOrg/neogit",
+    dependencies = {
+      "nvim-lua/plenary.nvim",         -- required
+      "nvim-telescope/telescope.nvim", -- optional
+      "sindrets/diffview.nvim",        -- optional
+      "ibhagwan/fzf-lua",              -- optional
+    },
+    config = true,
+    event = "VeryLazy",
+    version = "*"
+  },
+  {
+    "APZelos/blamer.nvim",
+    event = "VeryLazy",
+  },
+  {
+    "phaazon/hop.nvim",
+    branch = "v2",
+    event = "VeryLazy",
+    config = function()
+      require 'hop'.setup {}
+    end,
+  },
+  -- ["github/copilot.vim"] = {}
 }
