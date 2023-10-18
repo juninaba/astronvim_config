6126cab45e001afd21c8579ac2de8293070daff9
juninaba <43479094+juninaba@users.noreply.github.com>
2023-10-18 22:51:17 +0900

    update user.lua

--- plugins/user.lua
+++ plugins/user.lua
@@ -33,5 +33,14 @@ return {
       require 'hop'.setup {}
     end,
   },
+  {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup({
+      })
+    end
+  }
   -- ["github/copilot.vim"] = {}
 }
