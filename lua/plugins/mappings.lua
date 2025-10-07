5dfd3de17972b5cc66e8b835fd844fa34aea589b
juninaba <43479094+juninaba@users.noreply.github.com>
2025-10-07 14:50:10 +0900

    update mappings

--- lua/plugins/mappings.lua
+++ lua/plugins/mappings.lua
@@ -13,8 +13,16 @@ return {
           ["<Leader>sj"] = { "<c-\\><c-n><c-w>j", desc = "Terminal down window navigation" },
           ["<Leader>sk"] = { "<c-\\><c-n><c-w>k", desc = "Terminal up window navigation" },
           ["<Leader>sl"] = { "<c-\\><c-n><c-w>l", desc = "Terminal right window navigation" },
+
+          -- terminal
           ["<C-f>"] = { "<cmd>ToggleTerm direction=float<cr>", desc = "Toggle floating terminal" },
           ["<C-v>"] = { "<cmd>ToggleTerm size=80 direction=vertical<cr>", desc = "Toggle vertical terminal" },
+
+          -- resize
+          ["<A-h>"] = { ":vertical resize +2<CR>" },
+          ["<A-l>"] = { ":vertical resize -2<CR>" },
+          -- ["<A-j>"] = { ":resize -2<CR>" },
+          -- ["<A-k>"] = { ":resize +2<CR>" },
 
           L = {
             function() require("astrocore.buffer").nav(vim.v.count1) end,
