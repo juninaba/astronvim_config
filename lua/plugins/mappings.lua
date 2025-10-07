8d94f174873a01f546f201c99451a2600e1c6b74
juninaba <43479094+juninaba@users.noreply.github.com>
2025-10-07 12:39:31 +0900

    delete avante and claude-code plug

--- lua/plugins/mappings.lua
+++ lua/plugins/mappings.lua
@@ -13,6 +13,8 @@ return {
           ["<Leader>sj"] = { "<c-\\><c-n><c-w>j", desc = "Terminal down window navigation" },
           ["<Leader>sk"] = { "<c-\\><c-n><c-w>k", desc = "Terminal up window navigation" },
           ["<Leader>sl"] = { "<c-\\><c-n><c-w>l", desc = "Terminal right window navigation" },
+          ["<C-f>"] = { "<cmd>ToggleTerm direction=float<cr>", desc = "Toggle floating terminal" },
+          ["<C-v>"] = { "<cmd>ToggleTerm size=80 direction=vertical<cr>", desc = "Toggle vertical terminal" },
 
           L = {
             function() require("astrocore.buffer").nav(vim.v.count1) end,
@@ -22,6 +24,10 @@ return {
             function() require("astrocore.buffer").nav(-vim.v.count1) end,
             desc = "Previous buffer",
           },
+        },
+        t = {
+          ["<C-f>"] = { "<cmd>ToggleTerm direction=float<cr>", desc = "toggle floating terminal" },
+          ["<C-v>"] = { "<cmd>ToggleTerm size=80 direction=vertical<cr>", desc = "toggle vertical terminal" },
         },
       },
     },
